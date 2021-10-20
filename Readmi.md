5aca2b933462c3fc10c35db877528e2eacd8f3be
miqueiasbelfort <miqueiasbelfort8323@gmail.com>
2021-10-20 19:32:18 -0300

    fist commit

---
+++ Readmi.md
@@ -0,0 +1,10 @@
+Calculadora de Corrente elétrica, ultilizando a equação do ensino medio 3° ano
+
+-> Não preencher todos os compos
+-> Preencha pelo menos dois compos
+
+-> Projeto pessoal simples para melhoramento da logica de programação
+
+-> Baseado na formaula de Calcular corrente eletrica i = ▲Q/▲T
+
+by Miqueias Belfort
---
+++ script.js
@@ -0,0 +1,26 @@
+function Calcular(){
+    let i = document.getElementById("corrente").value;
+    let Q = document.getElementById("deltaCarga").value;
+    let T = document.getElementById("deltaTempo").value;
+
+    let txt = document.getElementById("txtReultado");
+
+    if (i,T,!Q){
+        let resultado = T * i;
+        txt.innerHTML = `Carga Elétrica = ${resultado} Coulombs (C)`;
+    } 
+    else if (Q,T && !i){
+        let resultado = Q / T;
+        txt.innerHTML = `Corrente Elétrica = ${resultado} Ampéres (A)`;
+    }
+    else if (i,Q && !T){
+        let resultado = Q/i;
+        txt.innerHTML = `Intervalo de tempo = ${resultado} Segundos (S)`;
+    }
+    if (!i && !Q && !T){
+        txt.innerHTML = "Preencha ao menos dois campos!!!"
+    }
+    else if (i && Q && T){
+        txt.innerHTML = "Não é possivel calcular <br/> com todos os campos preenchidos!!!"
+    }
+}
