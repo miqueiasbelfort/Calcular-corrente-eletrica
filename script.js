b9b0fef1dd2273031fb9bc88cd11c1d9e40fefc5
miqueiasbelfort <miqueiasbelfort8323@gmail.com>
2021-10-21 19:49:18 -0300

    new script

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 function Calcular(){
-    let i = document.getElementById("corrente").value;
-    let Q = document.getElementById("deltaCarga").value;
-    let T = document.getElementById("deltaTempo").value;
+
+    let iCorrente = document.getElementById("corrente").value;
+    let qCarga = document.getElementById("deltaCarga").value;
+    let tempo = document.getElementById("deltaTempo").value;
 
     let txt = document.getElementById("txtResultado");
 
+    //Novo código utilizando function
+
+    let func = function (i=0,q=0,t=0){
+        if(i && q && t==0){return q/i}
+
+        else if (i && q==0 && t){return t*i} 
+
+        else if (i==0 && q && t){return q/t}
+
+        else if (i==0 && q==0 && t==0){return "Não á números para efetuar o calculo!!!"}
+        
+        else if (i && q && t){return "Não é possivel efetuar o calculo <br/> com todos os campos preenchidos!!!"}
+    }
+
+    txt.innerHTML = `${func(iCorrente,qCarga,tempo)}`;
+
+    //Primeiro código
+    /*
     if (i,T,!Q){
         let resultado = T * i;
         txt.innerHTML = `Carga Elétrica = ${resultado} Coulombs (C)`;
@@ -23,4 +42,5 @@ function Calcular(){
     else if (i && Q && T){
         txt.innerHTML = "Não é possivel calcular <br/> com todos os campos preenchidos!!!"
     }
+    */
 }
